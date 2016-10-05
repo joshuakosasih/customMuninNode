ac1891097256fef00c5292fc3f9882069cc376f8
albertusk15@gmail.com <albertusk15@gmail.com>
2016-10-05 20:50:45 +0700

    fixing

--- server-th.c
+++ server-th.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -27,14 +29,14 @@ int main(int argc , char *argv[])
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1)
     {
-        printf("Could not create socket");
+        printf("Could not create socket\n");
     }
-    puts("Socket created");
+    printf("Socket created\n");
      
     // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(9999);
+    server.sin_port = htons(4949);
      
     // Bind
     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
@@ -43,18 +45,19 @@ int main(int argc , char *argv[])
         perror("bind failed. Error");
         return 1;
     }
-    puts("bind done");
+    
+    printf("Bind done\n");
      
     // Listen
     listen(socket_desc , 3);
      
     // Accept and incoming connection
-    puts("Waiting for incoming connections...");
+    printf("Waiting for incoming connections...\n");
     c = sizeof(struct sockaddr_in);
      
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
-        puts("Connection accepted");
+        printf("Connection accepted\n");
          
         pthread_t sniffer_thread;
         new_sock = malloc(1);
@@ -68,10 +71,8 @@ int main(int argc , char *argv[])
          
         // Join the thread
         //pthread_join( sniffer_thread , NULL);
-        puts("Handler assigned");
+        printf("Handler assigned\n");
     
-		//pthread_join( sniffer_thread , NULL);
-        
     }
      
     if (client_sock < 0)
@@ -79,7 +80,7 @@ int main(int argc , char *argv[])
         perror("accept failed");
         return 1;
     }
-     
+    
     return 0;
 }
  
@@ -90,249 +91,198 @@ int main(int argc , char *argv[])
  */
 void *connection_handler(void *socket_desc)
 {
-    // Get the socket descriptor
+    // get the socket descriptor
     int sock = *(int*)socket_desc;
+    
+    // how many characters are received
     int read_size;
-    char *message;
+    
+    // message to send
+    char *respond_message;
+    
+    // message from munin-master
     char client_message[2000];
      
-     
-    // Send some messages to the client
-	message = "I am your connection handler\n";
-    write(sock , message , strlen(message));
-     
-    message = "Now type something\n";
-    write(sock , message , strlen(message));
     
     
-    // Get hostname & send banner
-    char hostname[200];
-    //char* banner = strdup("# munin node at ");
-	char banner[] = "# munin node at ";
+	// get hostname 
+	char hostname[200];
 	
-    if ((gethostname(hostname, 200)) == 0) {	
-		strcat(banner, hostname);
-		strcat(banner, "\n");	
-		if (send(sock, banner, strlen(banner)+1, 0) < 0) {
-			
-			puts("Error: cannot send banner");
+	if ((gethostname(hostname, 200)) == 0) {
+		asprintf(&respond_message, "# munin node at %s\n", hostname);
+		if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+			printf("Error: cannot send banner\n");
 		}
 		printf("You are logged in to: %s\n", hostname);
-    } else {
-		puts("Error: cannot get hostname");
-    }
+	} else {
+		printf("Error: cannot get hostname\n");
+	}
+	
  
- 
     
-    // special case for list => input: list or list hostname
-    char list_full_msg[10];
-    sprintf(list_full_msg, "%s%s%s", "list ", hostname, "\n");
-    
+    // special case for list => input: list hostname
+    char *list_full_msg;
+    asprintf(&list_full_msg, "list %s", hostname);
     
     
-    // Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    
+    // receive a message from client
+    while((read_size = recv(sock, client_message, 2000 , 0)) > 0 )
     {
-        
-        // Send the message back to client
-    
 		
+		// gives null terminated
+		client_message[strlen(client_message)-1] = '\0'; 
+		
+		
+		// show the message from master
+		printf("Received from master: %s - %ld\n", client_message, strlen(client_message));
+        
+        
+        // send the message back to client
 		char* config_result[12];
 		
-		if (strcmp(client_message, "cap\n") == 0) {
+		if (strcmp(client_message, "cap") == 0 || strcmp(client_message, "cap multigraph dirtyconfig") == 0) {
 			
-			if (send(sock, "cap multigraph dirtyconfig\0\n", strlen("cap multigraph dirtyconfig") + 2, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
+			asprintf(&respond_message, "cap multigraph dirtyconfig\n");
+			
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: cap\n");
 			}	
 			
 		}
 		
-		else if (strcmp(client_message, "nodes\n") == 0) {
+		else if (strcmp(client_message, "nodes") == 0) {
 
+			asprintf(&respond_message, "%s\n.\n", hostname);
 			
-			char nodes_msg[50];
-			
-			sprintf(nodes_msg, "%s", hostname);
-			
-			if (send(sock, nodes_msg, strlen(nodes_msg), 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}		
-			
-			
-			if (send(sock, "\n.\0\n", 4, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: nodes\n");
 			}		
 			
 		}
 
-		else if (strcmp(client_message, "list\n") == 0 || strcmp(client_message, list_full_msg) == 0) {
-		
+		else if (strcmp(client_message, "list") == 0 || strcmp(client_message, list_full_msg) == 0) {
 		
 			// lists items available for gathering for this host.
 			// E.g. load, cpu, memory, df, etalia.
 			// If no host is given, default to host that runs the munin-node
 		
-			char list_msg[10];
+			asprintf(&respond_message, "%s\n", "memory");
 			
-			sprintf(list_msg, "%s", "memory");
-		
-			if (send(sock, list_msg, strlen(list_msg), 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: list\n");
 			}		
 			
-			
-			if (send(sock, "\n", 1, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}		
-		
-	
 		}
 	
-		else if (strcmp(client_message, "config memory\n") == 0) {
+		else if (strcmp(client_message, "config") == 0 || strcmp(client_message, "fetch") == 0) {
+			
+			asprintf(&respond_message, "%s\n.\n", "# Unknown service");
+			
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: config or fetch only\n");
+			}		
+				
+		}
+	
+		else if (strcmp(client_message, "config memory") == 0) {
+			
 			// call memory plugin to get the config data
 			CONFIG_MEMORY(config_result);
+		
 
-			
 			for (int idx = 0; idx < 12; idx++) {
-				
-				int pjg_conv = strlen(config_result[idx])+1;
-				
-				if (send(sock, config_result[idx], pjg_conv, 0) < 0) {
-					printf("send failed\n");
-					exit(-1);
+
+				if (idx < 11) {
+					asprintf(&respond_message, "%s\n", config_result[idx]);
+				} else {
+					asprintf(&respond_message, "%s\n.\n", config_result[idx]);
 				}
-				
-			}
-			
-			if (send(sock, ".\0\n", 3, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
+
+				if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+					printf("Send failed: config memory\n");
+				}
+							
 			}
 			
 		}
 		
-		else if (strcmp(client_message, "fetch memory\n") == 0) {
+		else if (strcmp(client_message, "fetch memory") == 0) {
 			
 			// call memory plugin to get the fetch data
 			int_fast64_t fetch_result[3];
+						
 			FETCH_MEMORY(fetch_result);
 
-			char fetch_str[50];
+
+			char fetch_str[50];			
 			char* fetch_msg[3];
 
 			fetch_msg[0] = "total.value ";
 			fetch_msg[1] = "used.value ";
 			fetch_msg[2] = "free.value ";
-
-			for (int i = 0;i < 3;i++) {
 			
-				printf("FETCHED %d - %ld\n", i, fetch_result[i]);
 			
-				sprintf(fetch_str, "%s%ld", fetch_msg[i], fetch_result[i]);
-				sprintf(fetch_str, "%s%s", fetch_str, "\0");
-				
-				if (send(sock, fetch_str, strlen(fetch_str)+1, 0) < 0) {
-					printf("Send failed\n");
-					exit(-1);
+			for (int i = 1;i < 3;i++) {
+			
+				//printf("FETCHED %d - %ld\n", i, fetch_result[i]);
+			
+				if (i == 1) {
+					asprintf(&respond_message, "%s%ld\n", fetch_msg[i], fetch_result[i]);
+				} else {
+					asprintf(&respond_message, "%s%ld\n.\n", fetch_msg[i], fetch_result[i]);
 				}
 				
-				
-				if (send(sock, "\n", strlen("\n")+1, 0) < 0) {
-					printf("Send failed\n");
-					exit(-1);
+				if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+					printf("Send failed: fetch memory\n");
 				}
-			
+				
 			}
 					
-			if (send(sock, ".\n", 2, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}
-				
 		}
 		
-		else if (strcmp(client_message, "version\n") == 0) {
+		else if (strcmp(client_message, "version") == 0) {
 		
-
-			char version_msg[100];
-
-			sprintf(version_msg, "%s%s%s", "Crazy node on ", hostname, " version: 0.1\0");
-
-			if (send(sock, version_msg, strlen(version_msg) + 1, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
+			asprintf(&respond_message, "%s%s%s\n", "Crazy node on ", hostname, " version: 0.1\0");
+		
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: version\n");
 			}		
 			
-			
-			if (send(sock, "\n", strlen("\n") + 1, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}		
 		} 
 		
-		else if (strcmp(client_message, "quit\n") == 0) {
+		else if (strcmp(client_message, "quit") == 0 || strcmp(client_message, "quit ") == 0) {
 
-			char quit_msg[] = "bye";
-			
-			if (send(sock, quit_msg, strlen(quit_msg), 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}
-
-			if (send(sock, "\n", 1, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}
-
-			close(socket_desc);
-			close(sock);
-
-			puts("Connection closed by client");
+			printf("Connection closed by client");
 			break;
 
 		} 
 		
 		else {
-			
-			char unknown_msg[100];
-	
-			sprintf(unknown_msg, "%s", "# Unknown command. Try cap, list, nodes, config, fetch, version or quit\0");
-	
-			if (send(sock, unknown_msg, strlen(unknown_msg) + 2, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}	
-			
-			
-			if (send(sock, "\n", strlen("\n") + 1, 0) < 0) {
-				printf("Send failed\n");
-				exit(-1);
-			}	
-		}
 		
+			asprintf(&respond_message, "%s\n", "# Unknown command. Try cap, list, nodes, config, fetch, version or quit");
+			
+			if (send(sock, respond_message, strlen(respond_message), 0) < 0) {
+				printf("Send failed: unknown command\n");
+			}	
+			
+		}
 		
 		memset(client_message, 0, sizeof(client_message));
 		 
     }
     
      
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
+    if(read_size == 0) {
+        printf("Client disconnected\n");
         fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
+    } else if(read_size == -1) {
+        perror("Receive failed");
     }
          
-    // Free the socket pointer
+    // free the socket pointer
     free(socket_desc);
-     
+    close(sock);
+    
     return 0;
 }
